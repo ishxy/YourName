40591f23a8551df9efbf1e74be34e284953456a1
caolu <chaolu@imudges.com>
2017-06-03 18:26:11 +0800

    1

--- app/src/main/java/ui/bottle/FriendList.java
+++ app/src/main/java/ui/bottle/FriendList.java
@@ -1,41 +1,116 @@
 package ui.bottle;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 import com.shxy.dazuoye.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 import bean.User;
+import cz.msebera.android.httpclient.Header;
+import global.Global;
+import util.HttpRequest;
+import view.BaseActivity;
 
 
-public class FriendList extends AppCompatActivity{
+public class FriendList extends BaseActivity {
 
     private ListView mListView;
     private ArrayList<User> mList;
     private MyAdapter adapter;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_friendlist);
         initView();
+
+
     }
 
     private void initView() {
         mListView = (ListView) findViewById(R.id.list);
-        adapter = new MyAdapter();
-        mListView.setAdapter(adapter);
+        setInfo();
     }
 
+    private void setInfo() {
+        final RequestParams params = new RequestParams();
+        params.add("userid", Global.MAIN_USER.getId() + "");
+        params.add("secretkey", Global.MAIN_USER.getSecretkey());
+        HttpRequest.get(getApplicationContext(), "get_contact_list", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String jsonString = new String(responseBody);
+                JsonParser parser = new JsonParser();
+                JsonObject object = (JsonObject) parser.parse(jsonString);
+                String msg = object.get("msg").getAsString();
+                Integer state = object.get("statues").getAsInt();
+                Log.i("jsonString",jsonString);
+                if (state == 0) {
+                    Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    JsonArray array = object.get("data").getAsJsonArray();
+                    mList = new ArrayList<User>();
+                    for (int i = 0; i < array.size(); i++) {
+                        JsonObject o = array.get(i).getAsJsonObject();
 
-    private class MyAdapter extends BaseAdapter{
+                        User user = new User();
+                        user.setUserphoto(o.get("userphoto").getAsString());
+                        user.setUsername(o.get("username").getAsString());
+                        user.setId(o.get("id").getAsInt());
+                        mList.add(user);
+                    }
+
+                    adapter = new MyAdapter();
+                    mListView.setAdapter(adapter);
+                    mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            Bundle bundle = new Bundle();
+                            bundle.putInt("id",mList.get(position).getId());
+
+                            Intent intent = new Intent(FriendList.this,UpBottleInfo.class);
+                            intent.putExtras(bundle);
+                            startActivity(intent);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(getApplicationContext(), "网络错误", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private class MyAdapter extends BaseAdapter {
 
         @Override
         public int getCount() {
@@ -54,7 +129,13 @@ public class FriendList extends AppCompatActivity{
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater.from(getApplicationContext()).inflate(R.layout.item_friendlist,null);
+            convertView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.item_friendlist, null);
+            ImageView img = (ImageView) convertView.findViewById(R.id.img);
+            TextView t = (TextView) convertView.findViewById(R.id.name);
+            t.setText(mList.get(position).getUsername());
+            Picasso.with(getApplicationContext())
+                    .load(mList.get(position).getUserphoto())
+                    .into(img);
             return convertView;
         }
     }
--- app/src/main/java/ui/bottle/UpBottleInfo.java
+++ app/src/main/java/ui/bottle/UpBottleInfo.java
@@ -1,5 +1,6 @@
 package ui.bottle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
@@ -11,11 +12,14 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.shxy.dazuoye.R;
 
+import bean.BaseBean;
 import cz.msebera.android.httpclient.Header;
 import global.Global;
 import util.HttpRequest;
@@ -24,13 +28,17 @@ import util.HttpRequest;
  * Created by caolu on 2017/5/26.
  */
 
-public class UpBottleInfo extends AppCompatActivity{
-    private ImageView back;
+public class UpBottleInfo extends AppCompatActivity {
+    private TextView back;
     private EditText contentEdit;
     private TextView upTextView;
+    private Integer targetId;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bundle bundle = getIntent().getExtras();
+        targetId = bundle.getInt("id", -1);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         setContentView(R.layout.ac_upbottleinfo);
@@ -38,7 +46,7 @@ public class UpBottleInfo extends AppCompatActivity{
     }
 
     private void initView() {
-        back = (ImageView) findViewById(R.id.back);
+        back = (TextView) findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,20 +64,28 @@ public class UpBottleInfo extends AppCompatActivity{
 
         });
     }
+
     private void up() {
+        if (targetId == -1)
+            return;
+
         RequestParams params = new RequestParams();
-        params.add("userid", Global.MAIN_USER.getId()+"");
-        params.add("secretkey",Global.MAIN_USER.getSecretkey());
-        params.add("bottlecontent",contentEdit.getText().toString());
+        params.add("userid", Global.MAIN_USER.getId() + "");
+        params.add("secretkey", Global.MAIN_USER.getSecretkey());
+        params.add("bottlecontent", contentEdit.getText().toString());
+        params.add("receiveuserid", targetId + "");
         HttpRequest.post(getApplicationContext(), "pushdriftingbottle", params, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-
+                Gson gson = new Gson();
+                BaseBean bean = gson.fromJson(new String(responseBody), BaseBean.class);
+                Toast.makeText(getApplicationContext(),bean.getMsg(),Toast.LENGTH_SHORT).show();
+                finish();
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
+                Toast.makeText(getApplicationContext(),new String(responseBody),Toast.LENGTH_SHORT).show();
             }
         });
     }
